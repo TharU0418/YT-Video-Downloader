9476005d32f811238403d0fbc08e2312c874407e
TharU0418 <101709936+TharU0418@users.noreply.github.com>
2023-06-22 21:37:22 +0530

    Add the extension route

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -159,6 +159,7 @@ function Home() {
                 </div>
                 
             </div>
+            
 
         </div>
     );
--- server/main.py
+++ server/main.py
@@ -8,6 +8,7 @@ import googleapiclient.discovery
 
 from pytube import YouTube
 
+import re
 import datetime
 
 app = Flask(__name__)
@@ -87,6 +88,71 @@ def getLink2():
 
 
 
+@app.route('/extenstion', methods=['GET'])
+def extentionResult():
+
+    print('Extenstion 📧📧')
+    input_link = request.args.get('userinput', default="",type=str)
+    
+
+    print('input_link',input_link)
+    usrLink = input_link
+    print("url",usrLink)
+
+    vid_id = get_video_id(usrLink)
+    vid_id = vid_id[0] if isinstance(vid_id, list) else vid_id  # Get the first element if it's a list
+    print(vid_id)
+
+    vid_title = get_video_title(vid_id)
+    print(vid_title)
+
+    thumbnail_url = get_video_img(vid_id)
+    print(thumbnail_url)
+
+    vquality = "144p"
+    
+
+    result = {
+        "URL": usrLink,
+        "Vid_title":vid_title,
+        "Thumbnail_url":thumbnail_url
+    }
+
+    #download_video(usrLink,vquality)
+    #print("down")
+
+    return jsonify(result)
+
+
+
+@app.route('/extenstion2', methods=['GET'])
+def extentionResul2t():
+
+    input_link = request.args.get('userinput', default="",type=str)
+    usrLink = input_link
+    vquality = request.args.get('video_quality', default="", type=str)
+
+
+    print(vquality)
+
+    vid_id = get_video_id(usrLink)
+    vid_id = vid_id[0] if isinstance(vid_id, list) else vid_id  # Get the first element if it's a list
+    print(vid_id)
+
+    vid_title = get_video_title(vid_id)
+    print(vid_title)
+
+    thumbnail_url = get_video_img(vid_id)
+    print(thumbnail_url)
+
+    #vquality = "144p"
+
+    download_video(usrLink,vquality)
+    print("down")
+
+
+
+
 x = datetime.datetime.now()
 
 @app.route('/test')
@@ -109,6 +175,8 @@ def get_video_id(yt_url):
         if "v" in query_params:
             return query_params["v"[0]]
         elif parsed_url.path.startswith("/embed"):
+            return parsed_url.path.split("/")[-1]
+        elif parsed_url.path.startswith("/watch"):
             return parsed_url.path.split("/")[-1]
         elif parsed_url.hostname == "youtu.be":
             return parsed_url.path[1:]
@@ -162,6 +230,18 @@ def download_video(video_id,vquality):
             print("Downloaded 👍.")
         else:
             print("No Video.")
+
+
+def download_video2(video_id):
+    ytObject = YouTube('8xlDwukxjnA')
+
+
+    video = ytObject.streams.get_audio_only()
+
+    video.download()
+    print("Downloaded.")     
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=os.getenv("PORT", default=5000))
